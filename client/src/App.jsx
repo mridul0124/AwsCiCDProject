4847448ad914b6413eb3231a6bd7fa84232d58dc
mridul0124 <panditmridul4@gmail.com>
2025-05-14 17:05:10 +0530

    created docker-compose file and nodejs port updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,11 @@ function App() {
   const [data, setData] = useState('');
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/data')
+    axios.get('http://localhost:8000/api/data')
       .then(res => setData(res.data.message))
       .catch(err => console.error(err));
   }, []);
-
+ 
   return (
     <div style={{ padding: '40px', fontFamily: 'Arial' }}>
       <h1>React + Vite Frontend Project</h1>
